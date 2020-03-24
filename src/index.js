c7dd1d3659a0ad259f58f04891a4d23ff0b455b9
Alisson Peres <aperes.dev@gmail.com>
2020-03-24 12:45:56 -0300

    Add localStorage

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import StartUp from './views';
-import project from './toDos';
+import StartUp from "./views";
+import project from "./toDos";
 
-const newProject = project('Todo-list');
-newProject.addNote('title', 'description', '01012020', 'urgent');
-newProject.addNote('title2', 'description2', '01012020', 'urgent');
-newProject.addNote('title3', 'description3', '01012020', 'urgent');
-newProject.addNote('title4', 'description4', '01012020', 'urgent');
+const set = (key, value) => localStorage.setItem(key, JSON.stringify(value));
+const get = key => localStorage.getItem(key);
+const remove = key => localStorage.removeItem(key);
 
-console.log(newProject.getNotes());
+const newProject = project("Todo-list");
+newProject.addNote("title", "description", "01012020", "urgent");
+newProject.addNote("title2", "description2", "01012020", "urgent");
+newProject.addNote("title3", "description3", "01012020", "urgent");
+newProject.addNote("title4", "description4", "01012020", "urgent");
+
+const newProject2 = project("Another List");
+newProject2.addNote("title", "description", "01012020", "urgent");
+newProject2.addNote("title2", "description2", "01012020", "urgent");
--- src/toDos.js
+++ src/toDos.js
@@ -1,17 +1,21 @@
-const project = (projectTitle) => {
+const set = (key, value) => localStorage.setItem(key, JSON.stringify(value));
+const get = key => localStorage.getItem(key);
+const remove = key => localStorage.removeItem(key);
+
+const project = projectTitle => {
   const title = projectTitle;
   const notes = [];
   const getNotes = () => notes;
   const addNote = (nTitle, description, dueDate, priority) => {
     const newNote = note(nTitle, description, dueDate, priority);
     notes.push(newNote);
+    set(title, notes);
   };
 
   return { title, addNote, getNotes };
 };
 
 const note = (nTitle, description, dueDate, priority) => {
-
   return { nTitle, description, dueDate, priority };
 };
 
