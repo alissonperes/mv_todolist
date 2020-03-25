a47cfc0358d7c795358e25f14a188dbb83d9e1d3
fedgut <fedgut@gmail.com>
2020-03-25 15:33:19 -0600

    add methods to project controller

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -1,14 +1,43 @@
 import ProjectModel from '../models/project.model';
+import TodosModel from '../models/todo.model';
 
 class ProjectController {
-  constructor(name, todos = []) {
-    this.projects = JSON.parse(localStorage.getItem('projects')) || ProjectModel('Default')
+  constructor() {
+    this.projects = JSON.parse(localStorage.getItem('projects'))
+    || ProjectModel('Default');
+  }
 
-    addProject(name) {
-      const newProject = ProjectModel(name);
-      this.projects.push(newProject);
-    };
-  };
-};
+  setStorage() {
+    localStorage.setItem('projects', JSON.stringify(this.projects));
+  }
+
+  addProject(name) {
+    const newProject = ProjectModel(name);
+    this.projects.push(newProject);
+    this.setStorage();
+  }
+
+  addTodo(projectId, name) {
+    const todo = TodosModel(name);
+    this.projects.find((p) => p.id === projectId).todos.push(todo);
+    this.setStorage();
+  }
+
+  editTodo(projectId, todoID, name, description, dueDate, priority) {
+    const project = this.projects.find((p) => p.id === projectId);
+    const todo = project.todos.find((t) => t.id === todoID);
+    todo.name = name;
+    todo.dueDate = dueDate;
+    todo.description = description;
+    todo.priority = priority;
+    this.setStorage();
+  }
+
+  removeTodo(projectId, todoID) {
+    const project = this.projects.find((p) => p.id === projectId);
+    project.todos = project.todos.filter((t) => t.id !== todoID);
+    this.setStorage();
+  }
+}
 
 export default ProjectController;
